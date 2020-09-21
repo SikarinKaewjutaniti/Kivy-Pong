b0650ce4926c10de3c945cf22a60850ba9d753ad
Sikarin <s6001012630152@email.kmutnb.ac.th>
2020-09-21 21:43:28 +0700

    adding Pong  Ball

--- Pong.py
+++ Pong.py
@@ -1,10 +1,30 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-    
+from kivy.properties import\
+    NumericProperty,\
+    ReferenceListProperty,\
+    ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
 class PongGame(Widget):
-	pass
+    pass
+
+
 class PongApp(App):
-        def build(self):
-        	return PongGame()
+    def build(self):
+        return PongGame()
+
+
 if __name__ == '__main__':
-	PongApp().run()
+    PongApp().run()
